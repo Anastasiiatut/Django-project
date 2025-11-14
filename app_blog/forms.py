6671e5955dee69f1d88fb9296d04d818395dafad
Anastasiia <batuieva.anastasiia@chnu.edu.ua>
2025-11-14 13:05:26 +0100

    image upload fix

--- app_blog/forms.py
+++ app_blog/forms.py
@@ -1,27 +1,10 @@
 from django import forms
+from django.forms.widgets import ClearableFileInput
+
 from .models import ArticleImage
 
-class MultipleFileInput(forms.ClearableFileInput):
-    allow_multiple_selected = True
-
-
-class MultipleFileField(forms.FileField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("widget", MultipleFileInput())
-        super().__init__(*args, **kwargs)
-
-    def clean(self, data, initial=None):
-        single_file_clean = super().clean
-        if isinstance(data, (list, tuple)):
-            result = [single_file_clean(d, initial) for d in data]
-        else:
-            result = [single_file_clean(data, initial)]
-        return result
-
-
-
 class ArticleImageForm(forms.ModelForm):
-    image = forms.ImageField(widget=MultipleFileInput())
+    image = forms.ImageField(widget=ClearableFileInput())
     class Meta:
         model = ArticleImage
         fields = '__all__'
