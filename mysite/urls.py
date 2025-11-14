a389b68112ac08e646efe374f68b8d48b63067fb
Anastasiia <batuieva.anastasiia@chnu.edu.ua>
2025-11-14 16:25:08 +0100

    styling with bootstrap

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from mysite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('app_blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
